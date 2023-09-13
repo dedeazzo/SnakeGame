3135fd09ebf92fe5e68d2233e0154ef708a9c40c
dedeazzo <daniel@exillion.com>
2023-09-13 14:08:56 +0200

    Initial commit

---
+++ app/src/main/java/com/example/snakegame/MainActivity.kt
@@ -0,0 +1,134 @@
+package com.example.snakegame
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.KeyEvent
+import android.view.View
+import android.widget.Button
+import android.widget.FrameLayout
+import androidx.core.view.isVisible
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val screenWidth = 600
+    private val screenHeight = 200
+    private val cellSize = 50
+
+    private val snake = Snake()
+    private val random = Random()
+    private val handler = Handler(Looper.getMainLooper())
+
+    private lateinit var gameFrame: FrameLayout
+    private lateinit var startButton: Button
+
+    private var currentDirection = Direction.RIGHT
+    private var foodX: Int = 0
+    private var foodY: Int = 0
+
+    private var isGameRunning = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        gameFrame = findViewById(R.id.game_frame)
+        startButton = findViewById(R.id.start_button)
+
+        startButton.setOnClickListener {
+            if (!isGameRunning) {
+                startGame()
+            }
+        }
+
+        // Register key event handling
+        gameFrame.isFocusable = true
+        gameFrame.isFocusableInTouchMode = true
+        gameFrame.requestFocus()
+        gameFrame.setOnKeyListener { _, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_DOWN) {
+                onKeyDown(keyCode, event)
+                return@setOnKeyListener true
+            }
+            false
+        }
+    }
+
+    private fun startGame() {
+        isGameRunning = true
+        startButton.isVisible = false
+
+        snake.reset()
+        spawnFood()
+
+        val gameRunnable = object : Runnable {
+            override fun run() {
+                snake.changeDirection(currentDirection) // Update snake's direction
+                updateGame()
+                handler.postDelayed(this, 100)
+            }
+        }
+        handler.postDelayed(gameRunnable, 100)
+    }
+
+    private fun updateGame() {
+        snake.move()
+
+        if (snake.checkCollision()) {
+            endGame()
+            return
+        }
+
+        if (snake.eatFood(foodX, foodY)) {
+            spawnFood()
+        }
+
+        gameFrame.removeAllViews()
+        drawSnake()
+        drawFood()
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        when (keyCode) {
+            KeyEvent.KEYCODE_DPAD_UP -> currentDirection = Direction.UP
+            KeyEvent.KEYCODE_DPAD_DOWN -> currentDirection = Direction.DOWN
+            KeyEvent.KEYCODE_DPAD_LEFT -> currentDirection = Direction.LEFT
+            KeyEvent.KEYCODE_DPAD_RIGHT -> currentDirection = Direction.RIGHT
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    private fun drawSnake() {
+        for (segment in snake.getSegments()) {
+            val segmentView = View(this)
+            segmentView.setBackgroundColor(ContextCompat.getColor(this, R.color.snake_color))
+            val params = FrameLayout.LayoutParams(cellSize, cellSize)
+            params.leftMargin = segment.x * cellSize
+            params.topMargin = segment.y * cellSize
+            gameFrame.addView(segmentView, params)
+        }
+    }
+
+    private fun drawFood() {
+        val foodView = View(this)
+        foodView.setBackgroundColor(ContextCompat.getColor(this, R.color.food_color))
+        val params = FrameLayout.LayoutParams(cellSize, cellSize)
+        params.leftMargin = foodX * cellSize
+        params.topMargin = foodY * cellSize
+        gameFrame.addView(foodView, params)
+    }
+
+    private fun spawnFood() {
+        foodX = random.nextInt(screenWidth / cellSize)
+        foodY = random.nextInt(screenHeight / cellSize)
+    }
+
+    private fun endGame() {
+        isGameRunning = false
+        startButton.isVisible = true
+        gameFrame.removeAllViews()
+    }
+}
---
+++ app/src/main/java/com/example/snakegame/Snake.kt
@@ -0,0 +1,106 @@
+package com.example.snakegame
+
+class Snake {
+    private val initialSize = 3
+    private val segments = mutableListOf<Segment>()
+    private var direction = Direction.RIGHT
+
+    init {
+        // Initialize the snake with segments
+        for (i in 0 until initialSize) {
+            segments.add(Segment(initialSize - 1 - i, 0))
+        }
+    }
+
+    fun reset() {
+        segments.clear()
+        direction = Direction.RIGHT
+        for (i in 0 until initialSize) {
+            segments.add(Segment(initialSize - 1 - i, 0))
+        }
+    }
+
+    fun move() {
+        val head = getHead()
+        val newHead = Segment(head.x, head.y)
+
+        when (direction) {
+            Direction.UP -> newHead.y--
+            Direction.DOWN -> newHead.y++
+            Direction.LEFT -> newHead.x--
+            Direction.RIGHT -> newHead.x++
+        }
+
+        // Add the new head to the front of the snake
+        segments.add(0, newHead)
+
+        // Remove the tail segment to keep the snake the same length
+        if (segments.size > initialSize) {
+            segments.removeAt(segments.size - 1)
+        }
+    }
+
+    fun changeDirection(newDirection: Direction) {
+        // Prevent the snake from reversing direction
+        if (newDirection != direction.opposite()) {
+            direction = newDirection
+        }
+    }
+
+    fun checkCollision(): Boolean {
+        val head = getHead()
+
+        // Check for collision with walls
+        val screenWidth = 600
+        val cellSize = 10
+        val screenHeight = 200
+        if (head.x < 0 || head.x >= screenWidth / cellSize || head.y < 0 || head.y >= screenHeight / cellSize) {
+            return true
+        }
+
+        // Check for collision with itself
+        for (i in 1 until segments.size) {
+            if (head == segments[i]) {
+                return true
+            }
+        }
+
+        return false
+    }
+
+    fun eatFood(foodX: Int, foodY: Int): Boolean {
+        val head = getHead()
+        if (head.x == foodX && head.y == foodY) {
+            // Snake ate the food, so add a new segment
+            segments.add(Segment(foodX, foodY))
+            return true
+        }
+        return false
+    }
+
+    fun getSegments(): List<Segment> {
+        return segments.toList()
+    }
+
+    private fun getHead(): Segment {
+        return segments[0]
+    }
+}
+
+data class Segment(var x: Int, var y: Int)
+
+enum class Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT;
+
+    fun opposite(): Direction {
+        return when (this) {
+            UP -> DOWN
+            DOWN -> UP
+            LEFT -> RIGHT
+            RIGHT -> LEFT
+        }
+    }
+}
